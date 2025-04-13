11dd5a79c01299244bfbc274df339ef97e682226
Wewiing <edwinosorio29092004@gmail.com>
2025-04-13 17:56:31 -0500

    Initial commit

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+  const saludo = 'hola'// debería marcar 'var' como incorrecto
+  console.log(saludo)
+  return (
+    <div>
+      <h1>Hola</h1>; {/* el punto y coma debería marcarlo como innecesario */}
+    </div>
+  )
+}
+
+export default Test
