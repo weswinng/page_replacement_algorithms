2ea20e890d600575fc57a480563700abcdaa5e27
Jonathan Gaviria <jonathan.gaviria@utp.edu.co>
2025-04-14 16:10:37 -0500

    Refactor FIFO function: improve logging and add LRU implementation with detailed frame tracking

--- src/utils/algorithms.js
+++ src/utils/algorithms.js
@@ -33,7 +33,7 @@ function FIFO(sequence, numPages) {
                 framesArrayTime[j]++;
             }
         }
-        console.log("Vector: ", framesArray , "Tiempos: ", framesArrayTime);
+        console.log("Frames Array: ", framesArray);
         resultMatrix.push([...framesArray]);
     }
     return {
@@ -42,6 +42,55 @@ function FIFO(sequence, numPages) {
     };
 }
 
+//console.log(FIFO("7 0 1 2 0 3 0 4 2 3", 3));
+
 // LRU
+function LRU(sequence, numPages) {
+    let sequenceArray = sequence.split(' ').map(Number);
+    let pageFaults = 0;
+    let framesArray = new Array(numPages).fill("-");
+    let resultMatrix = [];
+    let framesArrayTime = []
+
+    for (let i = 0; i < sequenceArray.length; i++) {
+        let page = sequenceArray[i];
+        if (!framesArray.includes(page)) {
+            pageFaults++;
+            if (framesArray.includes("-")){
+                for (let j = 0; j < framesArray.length; j++){
+                    if (framesArray[j] == "-"){
+                        framesArray[j] = page;
+                        break;
+                    }
+                }
+            } else {
+                framesArrayTime = []
+                for (let j = i - 1; j >= 1; j--){
+                    if (framesArrayTime.length < framesArray.length - 1){
+                        if (!framesArrayTime.includes(sequenceArray[j])){
+                            framesArrayTime.push(sequenceArray[j]);
+                        } 
+                    }
+                }
+                let indexTime
+                for (let j = 0; j < framesArray.length; j++){
+                    if (!framesArrayTime.includes(framesArray[j])){
+                        indexTime = framesArray.indexOf(framesArray[j]);
+                        framesArray[indexTime] = page;
+                        break;
+                    }
+                }
+            }
+        }
+        console.log("Frames Array: ", framesArray, "Frames Array Time: ", framesArrayTime);
+        resultMatrix.push([...framesArray]);
+    }
+    return {
+        pageFaults,
+        resultMatrix
+    };
+}
+
+console.log(LRU("7 0 1 2 0 3 0 4 2 3", 3));
 
 // OPT
