6885e1cc43899728445706d8cffe082385de5974
weswinng <edwin.osorio@utp.edu.co>
2025-04-16 15:58:50 -0500

    Improve FaultsSummary and SimulationTable components: add sequence check in FaultsSummary and display message when no results are available in SimulationTable

--- src/components/FaultsSummary.jsx
+++ src/components/FaultsSummary.jsx
@@ -4,44 +4,51 @@ import { usePageReplacement } from '../context/PageReplacementContext'
 
 const FaultsSummary = ({ selectedAlgorithm }) => {
   const { sequence, frames } = usePageReplacement()
-  const faultsFifo = FIFO(sequence, frames).pageFaults
-  const faultsLru = LRU(sequence, frames).pageFaults
-  const faultsOpt = OPT(sequence, frames).pageFaults
-  const faultsFifoPlus = FIFOplus(sequence, frames).pageFaults
+
+  // Verificar si sequence está vacío
+  const isSequenceEmpty = !sequence.trim()
+
+  const faultsFifo = !isSequenceEmpty ? FIFO(sequence, frames).pageFaults : null
+  const faultsLru = !isSequenceEmpty ? LRU(sequence, frames).pageFaults : null
+  const faultsOpt = !isSequenceEmpty ? OPT(sequence, frames).pageFaults : null
+  const faultsFifoPlus = !isSequenceEmpty ? FIFOplus(sequence, frames).pageFaults : null
 
   return (
     <>
       <ul>
         <li className='text-center text-lg font-bold'>Resumen de fallos</li>
-        <li
-          className={`text-center text-lg font-bold px-4 py-2 rounded ${
-            selectedAlgorithm === 'optimo' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-          }`}
-        >
-          OPT: {faultsOpt}
-        </li>
-        <li
-          className={`text-center text-lg font-bold px-4 py-2 rounded ${
-            selectedAlgorithm === 'fifo' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-          }`}
-        >
-          FIFO: {faultsFifo}
-        </li>
-        <li
-          className={`text-center text-lg font-bold px-4 py-2 rounded ${
-            selectedAlgorithm === 'lru' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-          }`}
-        >
-          LRU: {faultsLru}
-        </li>
-
-        <li
-          className={`text-center text-lg font-bold px-4 py-2 rounded ${
-            selectedAlgorithm === 'fifo+' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-          }`}
-        >
-          FIFO+: {faultsFifoPlus}
-        </li>
+        {!isSequenceEmpty && (
+          <>
+            <li
+              className={`text-center text-lg font-bold px-4 py-2 rounded ${
+                selectedAlgorithm === 'optimo' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+              }`}
+            >
+              OPT: {faultsOpt}
+            </li>
+            <li
+              className={`text-center text-lg font-bold px-4 py-2 rounded ${
+                selectedAlgorithm === 'fifo' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+              }`}
+            >
+              FIFO: {faultsFifo}
+            </li>
+            <li
+              className={`text-center text-lg font-bold px-4 py-2 rounded ${
+                selectedAlgorithm === 'lru' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+              }`}
+            >
+              LRU: {faultsLru}
+            </li>
+            <li
+              className={`text-center text-lg font-bold px-4 py-2 rounded ${
+                selectedAlgorithm === 'fifo+' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+              }`}
+            >
+              FIFO+: {faultsFifoPlus}
+            </li>
+          </>
+        )}
       </ul>
     </>
   )
