ef65334658de076d060c4686d09e86176daddb1c
weswinng <edwin.osorio@utp.edu.co>
2025-04-16 21:19:13 -0500

    chore: update tailwindcss to version 4.1.4 and enhance styling
    - Updated tailwindcss dependency in package.json to version 4.1.4.
    - Modified App component to include Tailwind CSS classes for improved heading styling.
    - Added Google Fonts import for 'Fira Sans Condensed' in index.css and defined custom theme colors.
    - Adjusted the order of plugins in vite.config.js for better compatibility.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Simulation from './components/Simulation'
 function App () {
   return (
     <>
-      <h1>Algoritmos de reemplazo de pagina</h1>
+      <h1 className='text-2xl font-bold text-center mb-4'>Algoritmos de reemplazo de pagina</h1>
       <InputForm />
       <Simulation />
     </>
