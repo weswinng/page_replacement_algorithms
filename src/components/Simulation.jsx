f59a1ff334863c1942cb19bda1c94e82c4cc863d
Jonathan Gaviria <jonathan.gaviria@utp.edu.co>
2025-04-17 16:15:01 -0500

    Refactor layout in Simulation component: change section to flex column and wrap articles in a div for improved structure and styling

--- src/components/Simulation.jsx
+++ src/components/Simulation.jsx
@@ -7,14 +7,16 @@ const Simulation = () => {
   const [selectedAlgorithm, setSelectedAlgorithm] = useState('optimo')
 
   return (
-    <section className='flex flex-row flex-wrap justify-center items-start gap-10'>
-      <article className='bg-background'>
-        <NavBarAlgorithms selectedAlgorithm={selectedAlgorithm} setSelectedAlgorithm={setSelectedAlgorithm} />
-        <SimulationTable selectedAlgorithm={selectedAlgorithm} />
-      </article>
-      <article>
-        <FaultsSummary selectedAlgorithm={selectedAlgorithm} />
-      </article>
+    <section className='flex flex-col justify-center items-center'>
+      <div className='flex flex-row flex-wrap justify-center gap-10 border-primary border-2 w-fit rounded-md p-4 m-4'>
+        <article className='bg-background'>
+          <NavBarAlgorithms selectedAlgorithm={selectedAlgorithm} setSelectedAlgorithm={setSelectedAlgorithm} />
+          <SimulationTable selectedAlgorithm={selectedAlgorithm} />
+        </article>
+        <article>
+          <FaultsSummary selectedAlgorithm={selectedAlgorithm} />
+        </article>
+      </div>
     </section>
   )
 }
