8e39b259f0ad0961ad4595cf93395e2d8766e41f
weswinng <edwin.osorio@utp.edu.co>
2025-04-17 23:09:58 -0500

    Enhance UI components: update styles in InputForm and Simulation for improved visual consistency; adjust layout in index.html and index.css for better structure

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -7,16 +7,27 @@ const InputForm = () => {
   const handleFramesChange = (e) => setFrames(Number(e.target.value))
 
   return (
-    <div className='text-primary p-4 m-2 rounded-md text-shadow text-shadow-primary font-primary'>
+    <div className='text-primary p-4 m-2 rounded-2xl text-shadow text-shadow-primary font-primary min-w-7xl border-1 border-primary shadow-md shadow-primary-shadow'>
       <h1 className='font-primary text-4xl font-black text-center text-primary'>Algoritmos de reemplazo de pagina</h1>
       <div className='flex flex-row mt-4 gap-8 max-w-[1000px] mx-auto text-lg'>
         <label className='flex flex-col gap-1 grow-1'>
           Secuencia de procesos:
-          <input className='border-2 border-primary rounded-md p-2' type='text' value={sequence} onChange={handleSequenceChange} placeholder='Ej: 7 0 1 2 0 3 0 4 2 3 0 3 2' />
+          <input
+            className='border-2 border-primary rounded-md p-2 focus:border-aquamarine focus:outline-none focus:shadow-sm focus:shadow-aquamarine cursor-text'
+            type='text'
+            value={sequence}
+            onChange={handleSequenceChange}
+            placeholder='Ej: 7 0 1 2 0 3 0 4 2 3 0 3 2'
+          />
         </label>
         <label className='flex flex-col gap-1 w-52'>
           Frames:
-          <input className='border-2 border-primary rounded-md p-2' type='text' value={frames} onChange={handleFramesChange} />
+          <input
+            className='border-2 border-primary rounded-md p-2 focus:border-aquamarine focus:outline-none focus:shadow-sm focus:shadow-aquamarine cursor-text'
+            type='text'
+            value={frames}
+            onChange={handleFramesChange}
+          />
         </label>
       </div>
     </div>
--- src/components/Simulation.jsx
+++ src/components/Simulation.jsx
@@ -7,7 +7,7 @@ const Simulation = () => {
   const [selectedAlgorithm, setSelectedAlgorithm] = useState('optimo')
 
   return (
-    <section className='flex flex-col justify-center items-center font-primary'>
+    <section className='flex flex-col justify-center items-center font-primary mt-4'>
       <div
         className='flex flex-row flex-wrap justify-center gap-10 border-primary border-2 w-fit rounded-2xl p-4 m-4 shadow-2xl shadow-primary-shadow bg-background'
       >
