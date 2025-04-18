9e4cc553ea3467c8ffcfdaeffecd0de07c033e6c
weswinng <edwin.osorio@utp.edu.co>
2025-04-18 15:13:32 -0500

    Refactor SimulationTable component: update table layout for improved responsiveness and enhance readability of results

--- src/components/SimulationTable.jsx
+++ src/components/SimulationTable.jsx
@@ -38,41 +38,48 @@ const SimulationTable = ({ selectedAlgorithm }) => {
   )
 
   return (
-    <div className='overflow-x-auto bg-background-light p-4 rounded-b-md rounded-r-md '>
-      <table className='table-auto '>
-        <thead>
-          <tr>
-            {sequence.split(' ').filter((item) => !isNaN(item) && item.trim() !== '').map((process, index) => (
-              <th key={index} className='text-primary transition-colors ease-in-out delay-1000 duration-1000'>
-                {process}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {transposedMatrix.map((row, rowIndex) => (
-            <tr key={`row-${rowIndex}`}>
-              {row.map((cell, cellIndex) => {
-                const hasAsterisk = String(cell[0]).includes('*') // Guarda la condición en una variable para claridad
-                return (
-                  <td
-                    key={`cell-${rowIndex}-${cellIndex}`}
-                    className={`border border-gray-300 p-2 w-16 h-16 text-center font-black text-lg justify-items-center transition-all ease-in-out duration-500 delay-75 ${
-                      cell[1] === 1 ? 'bg-aquamarine text-background' : 'text-primary'
-                    }`}
+    <div className='overflow-x-auto bg-background-light p-4 rounded-b-md rounded-r-md max-w-[930px]'>
+      <div className='w-full overflow-x-scroll'>
+        <table className='table-fixed w-full'>
+          <thead>
+            <tr>
+              {sequence
+                .split(' ')
+                .filter((item) => !isNaN(item) && item.trim() !== '')
+                .map((process, index) => (
+                  <th
+                    key={index}
+                    className='text-primary w-16 text-center transition-colors ease-in-out delay-1000 duration-1000'
                   >
-                    <div>{hasAsterisk ? cell[0][0] : cell[0]}</div>
-
-                    {hasAsterisk && (
-                      <svg xmlns='http://www.w3.org/2000/svg' width={20} height={20} viewBox='0 0 24 24' fill='currentColor' className='icon icon-tabler icons-tabler-filled icon-tabler-heart block align-middle'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z' /></svg>
-                    )}
-                  </td>
-                )
-              })}
+                    {process}
+                  </th>
+                ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {transposedMatrix.map((row, rowIndex) => (
+              <tr key={`row-${rowIndex}`}>
+                {row.map((cell, cellIndex) => {
+                  const hasAsterisk = String(cell[0]).includes('*') // Guarda la condición en una variable para claridad
+                  return (
+                    <td
+                      key={`cell-${rowIndex}-${cellIndex}`}
+                      className={`border border-gray-300 p-2 w-16 h-16 text-center font-black text-lg justify-items-center transition-all ease-in-out duration-500 delay-75 ${
+                        cell[1] === 1 ? 'bg-aquamarine text-background' : 'text-primary'
+                      }`}
+                    >
+                      <div>{hasAsterisk ? cell[0][0] : cell[0]}</div>
+                      {hasAsterisk && (
+                        <svg xmlns='http://www.w3.org/2000/svg' width={20} height={20} viewBox='0 0 24 24' fill='currentColor' className='icon icon-tabler icons-tabler-filled icon-tabler-heart block align-middle'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z' /></svg>
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
